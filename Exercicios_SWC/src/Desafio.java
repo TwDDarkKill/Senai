b6dd8b73cd6663f8264420132bd513104d9e1f27
TwDDarkKill <Niltondark53@hotmail.com>
2023-05-04 21:54:14 -0300

    Calculadora

--- Exercicios_SWC/src/Desafio.java
+++ Exercicios_SWC/src/Desafio.java
@@ -4,13 +4,23 @@ import java.util.Scanner;
 public class Desafio {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Insira um valor");
+        System.out.print("Insira um valor : ");
         while (scanner.hasNext()) {
             try {
                 int valor = scanner.nextInt();
+                if (valor!=0){
+                System.out.println("Conversão de dados concluida");
+                    System.out.print("Insira um valor : ");
+                }else{
+                    System.out.println("Encerrando aplicação");
+                    System.exit(0);
+                }
             } catch (InputMismatchException e) {
-                System.out.println("Erro : "+e.getMessage());
+                System.out.printf("Erro na conversão: " + e + "%n");
+                System.out.print("Insira um valor : ");
                 scanner.next(); // limpa o buffer de entrada
+            }catch (Exception e){
+                System.out.println("Encerrando aplicação");
             }
         }
     }
