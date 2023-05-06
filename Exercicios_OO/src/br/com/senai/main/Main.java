5a874a8a8b310143593a7936d66e37a452e7fd82
TwDDarkKill <Niltondark53@hotmail.com>
2023-05-05 21:05:47 -0300

    Calculadora

--- Exercicios_OO/src/br/com/senai/main/Main.java
+++ Exercicios_OO/src/br/com/senai/main/Main.java
@@ -5,16 +5,22 @@ public class Main {
     public static void main(String[]args){
 
         //Criando um objeto
+        //Para entrar Opções/Refactor/Encapsulate fields
 
         Veiculo corsa  = new Veiculo();
-        corsa.ano = 1998;
-        corsa.marca = "Chevrolet";
-        corsa.Ligar();
+        corsa.setModelo("corsa");
+        corsa.setAno(1998);
+        corsa.setMarca("Chevrolet");
+      // corsa.Ligar();
+
+        System.out.println(corsa.getMarca());
+        System.out.println(corsa.getAno());
 
         Veiculo Opala = new Veiculo();
-        Opala.ano = 1978;
-        Opala.modelo = "Opala";
-        Opala.marca = "Chevrolet";
-        Opala.Desligar();
+        Opala.setAno(1978);
+        Opala.setModelo("Opala");
+        Opala.setMarca("Chevrolet");
+       System.out.println(Opala.getModelo());
+       System.out.println(Opala.getAno());
     }
 }
--- Exercicios_OO/src/br/com/senai/modelo/Veiculo.java
+++ Exercicios_OO/src/br/com/senai/modelo/Veiculo.java
@@ -1,8 +1,8 @@
 package br.com.senai.modelo;
 public class Veiculo {//Criando atributos
-    public  String modelo;
-    public String marca;
-    public int ano;
+    private String modelo;
+    private String marca;
+    private int ano;
 
     //Criando os metodos
     public void Ligar(){
@@ -14,4 +14,27 @@ public class Veiculo {//Criando atributos
     }
 
 
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+//Para entrar Opções/Refactor/Encapsulate fields
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
 }
